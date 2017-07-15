80dcabe019423bfe769c770d0b3ef30906a57180
Ruy Asan <ruy.asan@gmail.com>
2017-07-15 16:40:54 -0700

    add babel loader, eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/app.jsx',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
@@ -12,6 +12,27 @@ module.exports = {
       title: 'BenchTest'
     })
   ],
+   module: {
+    rules: [{
+      test: /\.less$/,
+      use: [{
+          loader: "style-loader" 
+      }, {
+          loader: "css-loader"
+      }, {
+          loader: "less-loader"
+      }],
+    }, {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }],
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
