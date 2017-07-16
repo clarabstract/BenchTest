ec600f4621c2f7d72480f8e0840c4f31c92386b2
Ruy Asan <ruy.asan@gmail.com>
2017-07-15 17:03:38 -0700

    hello world

---
+++ src/Main.jsx
@@ -0,0 +1,5 @@
+import {createElement} from 'react';
+
+export default function Main () {
+  return <div>hello world and so on</div>;
+}
---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import {createElement} from 'react';
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import Main from './Main.jsx';
+const store = createStore(state => {
+  return state;
+});
+const rootEl = document.createElement('div');
+document.body.appendChild(rootEl);
+render(
+  <Provider store={store}>
+    <Main />
+  </Provider>,
+  rootEl
+);
